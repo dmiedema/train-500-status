e7dfb340fb71c550e6419d3d23b70cf5ffdcd169
dmiedema <danielmiedema+github@me.com>
2016-06-29 15:24:01 -0700

    initial pass
    It works and loads stuff. Now to make it tweet

--- fields.js
+++ fields.js
@@ -1,29 +1,29 @@
 /// Website
 // https://tickets.amtrak.com/itd/amtrak
-
+console.log("I'm fields.js and I'm a file.");
 // Change tab _first_
-var boxSegmentControl = document.getElementById('ff_tabbar');
+// var boxSegmentControl = document.getElementById('ff_tabbar');
 // boxSegmentControl.childElements().each(function(e) { e.removeClassName('active'); });
-var statusTab = document.getElementById('ff_tabbar_status');
+// var statusTab = document.getElementById('ff_tabbar_status');
 // statusTab.addClassName('active');
 
 //
 /// Request
-var stationInput = document.getElementById('status_from'); // ALY
-var trailNumber  = document.getElementById('status_train_num'); // 500
-var dateInput    = document.getElementById('wdfdate10'); // MM/DD/YYYY
+// var stationInput = document.getElementById('status_from'); // ALY
+// var trailNumber  = document.getElementById('status_train_num'); // 500
+// var dateInput    = document.getElementById('wdfdate10'); // MM/DD/YYYY
 
-var submitDiv    = document.getElementById('ff_status_submit_wrapper'); // Submit Div
-// submitDiv.childElements().last().click();
+// var submitDiv    = document.getElementById('ff_status_submit_wrapper'); // Submit Div
+// // submitDiv.childElements().last().click();
 
-/// Response
-var responseDiv = document.getElementById('resp_by_train_num_status_details');
+// /// Response
+// var responseDiv = document.getElementById('resp_by_train_num_status_details');
 
-var cityName           = document.getElementsByClassName('cityName'); // Content should be ALY
-// var delayedMsg         = document.getElementsByClassName('delayedMsg'); // something
-// var onTimeMsg          = document.getElementsByClassName('ontimeMsg'); // something if on time
-var scheduledDeparture = document.getElementsByClassName('scheduledArriveDepartMsg');
-var actualDeparture    = document.getElementsByClassName('arriveDepartTime');
+// var cityName           = document.getElementsByClassName('cityName'); // Content should be ALY
+// // var delayedMsg         = document.getElementsByClassName('delayedMsg'); // something
+// // var onTimeMsg          = document.getElementsByClassName('ontimeMsg'); // something if on time
+// var scheduledDeparture = document.getElementsByClassName('scheduledArriveDepartMsg');
+// var actualDeparture    = document.getElementsByClassName('arriveDepartTime');
 
 // cityName[0].innerText
 // delayedMsg[0].innerText
